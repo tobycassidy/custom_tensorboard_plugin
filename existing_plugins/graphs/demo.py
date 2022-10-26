31729b92776a9064a7a47b40d0418379e28efdd5
Toby Cassidy <tobycassidy@Tobys-MacBook-Pro.local>
2022-10-26 09:23:29 +0100

    guarding demo scripts and updating typehints

--- existing_plugins/graphs/demo.py
+++ existing_plugins/graphs/demo.py
@@ -4,14 +4,14 @@ import tensorflow as tf
 
 
 @tf.function
-def init_model(model_to_init: tf.keras.models.Model, data_to_init_model: tf.python.framework.ops.EagerTensor):
+def init_model(model_to_init: tf.keras.models.Model, data_to_init_model: tf.Tensor):
     """Function to initialise model with data utilising the autograph decorator @tf.function."""
     model_to_init(data_to_init_model)
 
 
 def write_graph_summaries(log_dir: str,
                           model_to_init: tf.keras.models.Model,
-                          data_to_init_model: tf.python.framework.ops.EagerTensor):
+                          data_to_init_model: tf.Tensor):
     """Function to write summaries and visualise tensorflow models using the GRAPHS plugin."""
     log_path = Path(log_dir)
 
@@ -29,26 +29,27 @@ def write_graph_summaries(log_dir: str,
         tf.summary.trace_export("Model Architecture", step=0)
 
 
-log_dir = "demo_logs"
-inputs = tf.keras.Input(shape=(3, 1), name="input")
-flatten = tf.keras.layers.Flatten(name="flatten")(inputs)
-dense1 = tf.keras.layers.Dense(32, name="dense1")(flatten)
-dense2 = tf.keras.layers.Dense(32, name="dense2")(dense1)
-output = tf.keras.layers.Dense(1, activation="sigmoid", name="sigmoid")(dense2)
+if __name__ == "__main__":
+    log_dir = "demo_logs"
+    inputs = tf.keras.Input(shape=(3, 1), name="input")
+    flatten = tf.keras.layers.Flatten(name="flatten")(inputs)
+    dense1 = tf.keras.layers.Dense(32, name="dense1")(flatten)
+    dense2 = tf.keras.layers.Dense(32, name="dense2")(dense1)
+    output = tf.keras.layers.Dense(1, activation="sigmoid", name="sigmoid")(dense2)
 
-model = tf.keras.Model(
-    inputs=inputs,
-    outputs=output
-)
-data = tf.random.normal(
-    mean=1.0,
-    stddev=0.1,
-    shape=(100, 3, 1)
-)
+    model = tf.keras.Model(
+        inputs=inputs,
+        outputs=output
+    )
+    data = tf.random.normal(
+        mean=1.0,
+        stddev=0.1,
+        shape=(100, 3, 1)
+    )
 
-# RUN
-write_graph_summaries(
-    log_dir=log_dir,
-    model_to_init=model,
-    data_to_init_model=data
-)
+    # RUN
+    write_graph_summaries(
+        log_dir=log_dir,
+        model_to_init=model,
+        data_to_init_model=data
+    )
--- existing_plugins/projector/demo.py
+++ existing_plugins/projector/demo.py
@@ -19,7 +19,7 @@ class TensorBoardProjector:
         self.tensor_filepath = os.path.join(log_dir, tensor_name + ".ckpt")
 
     @staticmethod
-    def register_embedding(log_dir, metadata_path, tensor_name):
+    def register_embedding(log_dir: str, metadata_path: str, tensor_name: str):
         config = projector.ProjectorConfig()
         embedding = config.embeddings.add()
         embedding.tensor_name = tensor_name
@@ -27,12 +27,12 @@ class TensorBoardProjector:
         projector.visualize_embeddings(log_dir, config)
 
     @staticmethod
-    def save_labels_tsv(log_dir, metadata_path, labels):
+    def save_labels_tsv(log_dir: str, metadata_path: str, labels: np.ndarray):
         with open(os.path.join(log_dir, metadata_path), "w") as f:
             for label in labels:
                 f.write('{}\n'.format(label))
 
-    def run(self, embedding, labels):
+    def run(self, embedding: pd.DataFrame, labels: np.ndarray):
         tensor_embeddings = tf.Variable(np.array(embedding), name=self.tensor_name)
         saver = tf.compat.v1.train.Saver([tensor_embeddings])
         saver.save(sess=None, global_step=0, save_path=self.tensor_filepath)
@@ -49,20 +49,21 @@ class TensorBoardProjector:
         print("Run `tensorboard --logdir={0}` to view result on tensorboard".format(self.log_dir))
 
 
-embedding = pd.concat([
-    pd.DataFrame(np.random.normal(0, 0.5, (1000, 10))),
-    pd.DataFrame(np.random.normal(3, 0.5, (1000, 10))),
-    pd.DataFrame(np.random.normal(5, 0.5, (1000, 10))),
-], axis=0)
+if __name__ == "__main__":
+    embedding = pd.concat([
+        pd.DataFrame(np.random.normal(0, 0.5, (1000, 10))),
+        pd.DataFrame(np.random.normal(3, 0.5, (1000, 10))),
+        pd.DataFrame(np.random.normal(5, 0.5, (1000, 10))),
+    ], axis=0)
 
-labels = np.vstack([
-    np.zeros((1000, 1)),
-    np.ones((1000, 1)),
-    np.add(np.ones((1000, 1)), 1),
-]).astype(int)
+    labels = np.vstack([
+        np.zeros((1000, 1)),
+        np.ones((1000, 1)),
+        np.add(np.ones((1000, 1)), 1),
+    ]).astype(int)
 
-viz = TensorBoardProjector(log_dir="demo_logs", metadata_path="meta.tsv", tensor_name="embeddings")
-viz.run(
-    embedding=embedding,
-    labels=labels
-)
+    viz = TensorBoardProjector(log_dir="demo_logs", metadata_path="meta.tsv", tensor_name="embeddings")
+    viz.run(
+        embedding=embedding,
+        labels=labels
+    )
